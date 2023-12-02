29ed3eff16e6927695c82eb587aad0bb8445737c
Eduardo Sanchez <e.gsanchez@outlook.com>
2023-12-01 23:31:12 -0600

    Cross Origin enabled to comunicate with frontend react

--- src/main/java/com/emgs0/movies/controller/ReviewController.java
+++ src/main/java/com/emgs0/movies/controller/ReviewController.java
@@ -5,10 +5,7 @@ import com.emgs0.movies.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -19,6 +16,7 @@ import java.util.Map;
  * Uses the serviceClass ReviewService
  */
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("api/v1/reviews")
 public class ReviewController {
     /**
